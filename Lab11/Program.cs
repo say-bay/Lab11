57bde317cdafc9b074ef513811db363462d9b48f
Алексей <@DESKTOP-V0RD6HJ>
2021-10-27 10:53:57 +0700

    Добавьте файлы проекта.

---
+++ Lab11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Equation x = new Equation();
+            x.k = -10;
+            x.b = 30;
+            x.Root();
+            Console.ReadKey();
+        }
+        struct Equation
+        {
+            public double k;
+            public double b;
+            public void Root()
+            {
+                Console.WriteLine(-(b / k));
+            }
+        }
+    }
+}
